3c03f72e8883c8a6589fbe172a1af7a2bb566387
Madhav S <98938384+Madhav-3006@users.noreply.github.com>
2026-06-25 12:47:29 +0530

    Update storage.c

--- firmware/storage.c
+++ firmware/storage.c
@@ -1,5 +1,31 @@
+#include <stdio.h>
+
 double energy = 0.0;
 
-save_energy();
+void save_energy()
+{
+    FILE *fp = fopen("/data/energy.dat", "w");
 
-load_energy();
+    if (fp)
+    {
+        fprintf(fp, "%.8f\n", energy);
+        fclose(fp);
+    }
+}
+
+void load_energy()
+{
+    FILE *fp = fopen("/data/energy.dat", "r");
+
+    if (fp)
+    {
+        fscanf(fp, "%lf", &energy);
+        fclose(fp);
+
+        printf("Loaded energy = %.6f kWh\n", energy);
+    }
+    else
+    {
+        energy = 0.0;
+    }
+}
